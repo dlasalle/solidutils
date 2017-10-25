3bad8a53fb7d92391ab7b6a435340fdf270d1993
Dominique LaSalle <dominique@solidlake.com>
2017-10-25 09:13:25 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+SolidUtils
+----------
+
+C++ utility classes.
+
+
+Usage
+-----
+
+This repository is meant to be used as a submodule in other projects.
+
+
+What's in here?
+---------------
+
+While the utilities in this repository provide functionality similar to some of
+that found the Standard Template Library, there should be no duplication.
+For example, the `Array` class found here provides a subset of the
+functionality of `std::vector`, with the exception that it allows allocation of
+elements within it without initialization/construction.
---
+++ test/FixedPriorityQueue_test.cpp
@@ -0,0 +1,76 @@
+/**
+ * @file FixedPriorityQueue_test.cpp
+ * @brief Unit tests for the FixedPriorityQueue class.
+ * @author Dominique LaSalle <dominique@solidlake.com>
+ * Copyright 2017, Solid Lake LLC
+ * @version 1
+ * @date 2017-10-20
+ */
+
+
+
+#include "UnitTest.hpp"
+#include "FixedPriorityQueue.hpp"
+
+
+namespace solidutils
+{
+
+
+UNITTEST(FixedPriortyQueue, AddPopInOrder)
+{
+  FixedPriorityQueue<float, int> pq(0, 10);
+
+  for (int i = 0; i < 10; ++i) {
+    pq.add(1.0/(i+1), i);    
+  }
+
+  for (int i = 0; i < 10; ++i) {
+    int const num = pq.pop();
+    testEqual(num, i);
+  }
+}
+
+
+UNITTEST(FixedPriortyQueue, AddPeek)
+{
+  FixedPriorityQueue<float, int> pq(0, 10);
+
+  for (int i = 0; i < 10; ++i) {
+    pq.add(1.0/(i+1), i);    
+  }
+
+  testEqual(pq.peek(), 0);
+}
+
+
+UNITTEST(FixedPriortyQueue, AddMax)
+{
+  FixedPriorityQueue<float, int> pq(0, 10);
+
+  for (int i = 0; i < 10; ++i) {
+    pq.add(1.0/(i+1), i);    
+  }
+
+  testEqual(pq.max(), 1.0);
+}
+
+
+UNITTEST(FixedPriortyQueue, AddPopReverseOrder)
+{
+  FixedPriorityQueue<float, int> pq(0, 10);
+
+  for (int i = 0; i < 10; ++i) {
+    pq.add(i/10.0, i);    
+  }
+
+  for (int i = 10; i > 0;) {
+    --i;
+    int const num = pq.pop();
+    testEqual(num, i);
+  }
+}
+
+
+
+}
