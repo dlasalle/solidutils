148a5d7c98c2ab2c1edf8f4667bdff0297e77993
Dominique LaSalle <dominique@solidlake.com>
2018-09-08 10:36:02 -0700

    Embedd license in each source file

--- solidutils/FixedSet.hpp
+++ solidutils/FixedSet.hpp
@@ -1,11 +1,29 @@
 /**
-* @file FixedSet.hpp
-* @brief The FixedSet class.
-* @author Dominique LaSalle <dominique@solidlake.com>
-* Copyright 2017, Solid Lake LLC
-* @version 1
-* @date 2017-10-19
-*/
+ * @file FixedSet.hpp
+ * @brief The FixedSet class.
+ * @author Dominique LaSalle <dominique@solidlake.com>
+ * Copyright 2017-2018, Solid Lake LLC
+ * @version 1
+ * @date 2017-10-19
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
 
 
 
@@ -54,7 +72,7 @@ class FixedSet
     *
     * @param element The element.
     *
-    * @return The element. 
+    * @return The element.
     */
     bool has(
         T const element) const noexcept
@@ -63,7 +81,7 @@ class FixedSet
 
       ASSERT_LESS(index, m_index.size());
 
-      return m_index[index] != NULL_INDEX; 
+      return m_index[index] != NULL_INDEX;
     }
 
     /**
@@ -79,12 +97,12 @@ class FixedSet
       ASSERT_LESS(index, m_index.size());
       ASSERT_EQUAL(m_index[index], NULL_INDEX);
 
-      m_data[m_size] = element; 
+      m_data[m_size] = element;
       m_index[index] = m_size;
 
       ++m_size;
     }
-     
+
 
     /**
     * @brief Remove an element from this set.
@@ -101,7 +119,7 @@ class FixedSet
 
       --m_size;
       T const swap = m_data[m_size];
-      size_t const place = m_index[index];       
+      size_t const place = m_index[index];
       m_data[place] = swap;
       m_index[swap] = place;
       m_index[index] = NULL_INDEX;
@@ -187,7 +205,7 @@ class FixedSet
     }
 
 
-  private: 
+  private:
     size_t m_size;
     Array<T> m_data;
     Array<T> m_index;
