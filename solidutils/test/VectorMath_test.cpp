b8ec47387f8e633c5dabcfc4a1e9e990a08d025f
Dominique LaSalle <dominique@solidlake.com>
2018-11-14 18:24:53 -0800

    Add unit test for iterator based exclusive prefix sum

--- solidutils/test/VectorMath_test.cpp
+++ solidutils/test/VectorMath_test.cpp
@@ -1,11 +1,29 @@
 /**
-* @file VectorMath_test.cpp
-* @brief Unit tests for the VectorMath class.
-* @author Dominique LaSalle <dominique@solidlake.com>
-* Copyright 2017, Solid Lake LLC
-* @version 1
-* @date 2017-10-11
-*/
+ * @file VectorMath_test.cpp
+ * @brief Unit tests for the VectorMath class.
+ * @author Dominique LaSalle <dominique@solidlake.com>
+ * Copyright 2017-2018, Solid Lake LLC
+ * @version 1
+ * @date 2017-10-11
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
 
 
 
@@ -47,7 +65,7 @@ UNITTEST(VectorMath, Increment)
 }
 
 
-UNITTEST(Array, PrefixSumExclusive)
+UNITTEST(Array, PrefixSumExclusivePointer)
 {
   std::vector<size_t> m(3UL);
 
@@ -62,6 +80,21 @@ UNITTEST(Array, PrefixSumExclusive)
   testEqual(m[2], 4u);
 }
 
+
+UNITTEST(Array, PrefixSumExclusiveIterator)
+{
+  std::vector<size_t> m(3UL);
+
+  m[0] = 3;
+  m[1] = 1;
+  m[2] = 2;
+
+  VectorMath::prefixSumExclusive(m.data(), m.data() + m.size());
+
+  testEqual(m[0], 0u);
+  testEqual(m[1], 3u);
+  testEqual(m[2], 4u);
+}
 
 
 
